08496b029c8553512b789f00101190187fac5325
Martin Acosta <tinchoz49@gmail.com>
2020-09-27 14:16:37 -0300

    minor change

--- browser/polyfills/process.js
+++ browser/polyfills/process.js
@@ -1,18 +1,22 @@
 const { default: _process } = require("bfs-process");
 
-// polyfill override global browserify "process" implementation: https://github.com/defunctzombie/node-process
-for (const prop in _process) {
-	if (typeof _process[prop] === "function") {
-		_process[prop].bind(_process);
-	}
-	if (prop === "env") {
-		continue;
-	}
-	process[prop] = _process[prop];
-}
-
 if (typeof queueMicrotask !== "undefined") {
-	process.nextTick = function nextTick(handler, ...args) {
+	_process.nextTick = function nextTick(handler, ...args) {
 		queueMicrotask(() => handler(...args));
 	};
 }
+
+if (typeof process !== "undefined") {
+	// polyfill override global browserify "process" implementation: https://github.com/defunctzombie/node-process
+	for (const prop in _process) {
+		if (typeof _process[prop] === "function") {
+			_process[prop].bind(_process);
+		}
+		if (prop === "env") {
+			continue;
+		}
+		process[prop] = _process[prop];
+	}
+}
+
+module.exports = _process;
